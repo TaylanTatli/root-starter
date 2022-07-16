b77a53fbb0ba7d29666185ab3b6da1d40bff5ff6
Taylan Tatlı <taylantatli90@gmail.com>
2022-07-17 01:43:46 +0300

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
-# root-starter
+# Root Startpage
 
-A simple and clean startpage star2
+Fork of [root-startpage](https://github.com/imreyesjorge/root-startpage) by [Jorge Reyes](https://github.com/imreyesjorge). 
+
+## Changes
+
+* Using Font Awesome 6 Free for icons now.
+* Weather feature is added. (You have to add your city id from [openweather](https://openweathermap.org/)
+* Using grid for card container instead of flex.
+* Cards are smaller to fit more cards to screen.
+* Card titles are visible for usability.
+
+> Using Turkish for my own use. You may need to edit some lines in `index.html` and `app.js` to use it properly.
+
+
+### Root is a start-page aimed to simplicity and elegance
+
+This project is blazing fast :zap:, it only contains an html, css and javascript files.
+
+## Customizing
+
+Root was created to be lightweight, fast and easy to customize.
+
+### Changin Colors
+
+To change the colors used in the startpage edit `styles.css`.
+
+```css
+:root {
+  --primary: #dd2e44;
+  --text-light: #eeeeee;
+  --background: #1e1c21;
+  --background-light: #333138;
+}
+```
+
+### Changin Name and Adding Custom Cards
+
+> This project uses [Remix Icons](https://remixicon.com/).
+
+To change the default name and edit the cards, edit `app.js`. It should look something like this:
+
+```js
+const NAME = "John Doe";
+
+const CARDS = [
+  {
+    name: "Twitter",
+    icon: "ri-twitter-fill",
+    link: "https://twitter.com",
+  },
+  {
+    name: "Github",
+    icon: "ri-github-fill",
+    link: "https://github.com/",
+  },
+];
+```
+
+To add a new card, just append a new object to the `CARDS` constant. The object should look something like this:
+
+```js
+{
+  name: "<Webpage Name>",
+  icon: "<Icon Class-Name>",
+  link: "<Webpage Link>"
+},
+```
+
+> To get the icon class-name, just go to [Remix Icons](https://remixicon.com/), select the desired icon. You should see something like this: `<i class="this-is-the-class-name"></i>`.
+
+To use 24 hour time simply change the time section of `app.js` to
+```
+// Update the Time
+  currentTime.innerHTML = `${currentHour}:${currentMinute}`;
+```
---
+++ app.js
@@ -0,0 +1,345 @@
+/*****************/
+/* EDITABLE INFO */
+/*****************/
+
+/* -------------------------------------------------------- */
+
+const NAME = "Taylan";
+
+const CARDS = [
+  {
+    name: "Trakt",
+    icon: "fa-tv",
+    link: "https://trakt.tv/",
+  },
+  {
+    name: "Reddit",
+    icon: "fa-reddit",
+    link: "https://www.troddit.com/r/unixporn/new",
+  },
+  {
+    name: "lobste.rs",
+    icon: "fa-shrimp",
+    link: "https://lobste.rs/",
+  },
+  {
+    name: "HackerNews",
+    icon: "fa-y",
+    link: "https://news.ycombinator.com/news",
+  },
+  {
+    name: "Lemmy",
+    icon: "fa-frog",
+    link: "https://lemmy.ml/",
+  },
+  {
+    name: "ProductHunt",
+    icon: "fa-p",
+    link: "https://www.producthunt.com/",
+  },
+  {
+    name: "Github",
+    icon: "fa-github",
+    link: "https://github.com/",
+  },
+  {
+    name: "Dizilla",
+    icon: "fa-clapperboard",
+    link: "https://dizilla5.com/",
+  },
+  {
+    name: "Pocket",
+    icon: "fa-get-pocket",
+    link: "https://app.getpocket.com/",
+  },
+  {
+    name: "Resmi Gazete",
+    icon: "fa-newspaper",
+    link: "https://resmigazete.gov.tr/",
+  },
+  {
+    name: "Netlify",
+    icon: "fa-diamond",
+    link: "https://app.netlify.com/",
+  },
+  {
+    name: "Google Drive",
+    icon: "fa-google-drive",
+    link: "https://drive.google.com/drive/",
+  },
+  {
+    name: "Blog",
+    icon: "fa-blog",
+    link: "https://taylantatli.com/",
+  },
+  {
+    name: "Books",
+    icon: "fa-book",
+    link: "https://books.taylantatli.com/",
+  },
+  {
+    name: "Notes",
+    icon: "fa-note-sticky",
+    link: "https://notes.taylantatli.com/",
+  },
+  {
+    name: "1000Kitap",
+    icon: "fa-book-atlas",
+    link: "https://1000kitap.com/",
+  },
+  {
+    name: "Adobe Docs",
+    icon: "fa-folder",
+    link: "https://documentcloud.adobe.com/",
+  },
+  {
+    name: "Dev.to",
+    icon: "fa-dev",
+    link: "https://dev.to/",
+  },
+  {
+    name: "Gitlab",
+    icon: "fa-gitlab",
+    link: "https://gitlab.com/",
+  },
+  {
+    name: "Youtube",
+    icon: "fa-youtube",
+    link: "https://www.youtube.com/",
+  },
+  {
+    name: "OSYM",
+    icon: "fa-school",
+    link: "https://ais.osym.gov.tr/",
+  },
+  {
+    name: "KAA",
+    icon: "fa-crown",
+    link: "https://www2.kickassanime.rs/",
+  },
+  {
+    name: "UserStyles",
+    icon: "fa-vest",
+    link: "https://33kk.github.io/uso-archive/?search=reddit&sort=updated",
+  },
+  {
+    name: "Quickref",
+    icon: "fa-asterisk",
+    link: "https://quickref.me/",
+  },
+  {
+    name: "Mdigi Tools",
+    icon: "fa-screwdriver-wrench",
+    link: "https://mdigi.tools/",
+  },
+  {
+    name: "Tagpacker",
+    icon: "fa-tags",
+    link: "https://tagpacker.com/",
+  },
+  {
+    name: "Raindrop",
+    icon: "fa-droplet",
+    link: "https://app.raindrop.io/my/0",
+  },
+  {
+    name: "fffuel",
+    icon: "fa-gas-pump",
+    link: "https://fffuel.co",
+  },
+  {
+    name: "Google Translate",
+    icon: "fa-language",
+    link: "https://translate.google.com/",
+  },
+  {
+    name: "DesignerNews",
+    icon: "fa-swatchbook",
+    link: "https://www.designernews.co/",
+  },
+  {
+    name: "Fosstodon",
+    icon: "fa-mastodon",
+    link: "https://fosstodon.org/web/timelines/home",
+  },
+  {
+    name: "Instagram",
+    icon: "fa-instagram",
+    link: "https://www.instagram.com/",
+  }
+];
+
+/* -------------------------------------------------------- */
+
+/******************/
+/* CLOCK FUNCTION */
+/******************/
+
+const DAYS = [
+  "Pazar",
+  "Pazartesi",
+  "Salı",
+  "Çarşamba",
+  "Perşembe",
+  "Cuma",
+  "Cumartesi",
+];
+
+const MONTHS = [
+  "Ocak",
+  "Şubat",
+  "Mart",
+  "Nisan",
+  "Mayıs",
+  "Haziran",
+  "Temmuz",
+  "Ağustos",
+  "Eylül",
+  "Ekim",
+  "Kasım",
+  "Aralık",
+];
+
+const updateDate = () => {
+  // Create a new Date object and get the complete Date/Time information
+  const completeDate = new Date();
+
+  // Time Variables
+  let currentHour = formatDigit(completeDate.getHours());
+  let currentMinute = formatDigit(completeDate.getMinutes());
+
+  // Date Variables
+  let currentDay = completeDate.getDay();
+  let currentNumber = completeDate.getDate();
+  let currentMonth = completeDate.getMonth();
+
+  // Update the Time
+  currentTime.innerHTML = `${currentHour}:${currentMinute}`;
+
+  // Update the Date
+  currentDate.innerHTML = `${currentNumber} ${MONTHS[currentMonth]}, ${DAYS[currentDay]}`;
+
+  // Greeting based on hour
+  let greeting = "";
+
+  if (currentHour >= 0 &&currentHour < 6)
+    greeting = "İyi Geceler"
+  else if (currentHour >= 6 &&currentHour < 12)
+    greeting = "Günaydın";
+  else if(currentHour == 12)
+    greeting = "Tünaydın";
+  else if (currentHour >= 12 && currentHour <= 17)
+    greeting = "İyi günler";
+  else if (currentHour >= 17 && currentHour <= 23)
+    greeting = "İyi akşamlar";
+  else 
+    greeting = "Merhaba";
+
+  greetingText.innerHTML = `${greeting}`;
+
+  // Create a Loop
+  setTimeout(() => {
+    updateDate();
+  }, 30000);
+};
+
+const formatDigit = (digit) => {
+  return digit > 9 ? `${digit}` : `0${digit}`;
+};
+
+/********************/
+/* WEATHER FUNCTION */
+/********************/
+
+const wIcons = {
+  "01d": "./icons/01d.svg",
+  "01n": "./icons/01n.svg",
+  "02d": "./icons/02d.svg",
+  "02n": "./icons/02n.svg",
+  "03d": "./icons/03d.svg",
+  "03n": "./icons/03n.svg",
+  "04d": "./icons/04d.svg",
+  "04n": "./icons/04n.svg",
+  "09d": "./icons/09d.svg",
+  "09n": "./icons/09n.svg",
+  "10d": "./icons/10d.svg",
+  "10n": "./icons/10n.svg",
+  "11d": "./icons/11d.svg",
+  "11n": "./icons/11n.svg",
+  "13d": "./icons/13d.svg",
+  "13n": "./icons/13n.svg",
+  "50d": "./icons/50d.svg",
+  "50n": "./icons/50n.svg",
+};
+
+const weather = {};
+weather.temperature = {};
+
+var tempUnit = 'C';
+
+const KELVIN = 272.15;
+const key = 'APIHERE';
+const cityID = "320879";
+getWeather(cityID);
+
+function getWeather(city) {
+  let api = `https://api.openweathermap.org/data/2.5/weather?id=${city}&lang=tr&appid=${key}`;
+  fetch(api)
+    .then(function(response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      let celsius = Math.floor(data.main.temp - KELVIN);
+      weather.temperature.value = tempUnit == 'C' ? celsius : (celsius * 9) / 5 + 32;
+      weather.description = data.weather[0].description;
+      weather.city = data.name;
+      weather.icon = data.weather[0].icon;
+    })
+    .then(function() {
+      displayWeather();
+    });
+}
+
+function displayWeather() {
+  weatherCity.innerHTML = weather.city;
+  weatherTemp.innerHTML = `${weather.temperature.value.toFixed(0)}°<span class="darkfg">${tempUnit}</span>`;
+  weatherDesc.innerHTML = weather.description;
+  weatherIcon.src = wIcons[weather.icon]
+}
+
+/******************/
+/* CARDS FUNCTION */
+/******************/
+
+const printCards = () => {
+  for (const card of CARDS) {
+    let currentCard = document.createElement("a");
+    let currentCardText = document.createElement("p");
+    currentCardText.appendChild(document.createTextNode(card.name));
+    let currentCardIcon = document.createElement("i");
+    currentCardIcon.classList.add(card.icon);
+
+    // Style the Card Element
+    currentCard.classList.add("card");
+    currentCard.href = card.link;
+
+    // Style the Icon
+    currentCardIcon.classList.add("card__icon");
+
+    // Style the Text
+    currentCardText.classList.add("card__name");
+
+    currentCard.append(currentCardIcon);
+    currentCard.append(currentCardText);
+    cardContainer.appendChild(currentCard);
+  }
+};
+
+/****************/
+/* STARTER CODE */
+/****************/
+
+userName.innerHTML = NAME;
+printCards();
+updateDate();
